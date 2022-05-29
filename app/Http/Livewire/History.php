9a63ec61536f44e70930368f6d231dffe605accb
Mujah Al Badri <mujahalbadri@gmail.com>
2022-05-29 15:51:52 +0700

    History section

--- app/Http/Livewire/History.php
+++ app/Http/Livewire/History.php
@@ -2,12 +2,27 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Order;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class History extends Component
 {
+    public $orders;
+
+    public function mount()
+    {
+        if (!Auth::user()) {
+            return redirect()->route('login');
+        }
+        $this->orders = Order::where('user_id', Auth::user()->id)->where('status', '!=', 0)->get();
+    }
+
+
     public function render()
     {
+
+
         return view('livewire.history');
     }
 }
--- resources/views/livewire/history.blade.php
+++ resources/views/livewire/history.blade.php
@@ -23,8 +23,70 @@
 		</div>
 	</div>
 
-	<div class="row">
+	<div class="row mt-4">
 		<div class="col">
+			<div class="table-responsive">
+				<table class="table text-center">
+					<thead>
+						<tr>
+							<td>No.</td>
+							<td>Tanggal Pesan</td>
+							<td>Kode Pemesanan</td>
+							<td>Pesanan</td>
+							<td>Status</td>
+							<td class="font-weight-bold">Total Harga</td>
+						</tr>
+					</thead>
+					<tbody>
+						@if (is_array($orders) || is_object($orders))
+						@forelse ($orders as $order)
+						<tr>
+							<td>{{ $loop->iteration }}</td>
+							<td>{{ $order->created_at }}</td>
+							<td>{{ $order->order_code }}</td>
+							<td>
+								<?php $order_details = \App\Models\OrderDetail::where('order_id', $order->id)->get(); ?>
+								@foreach ($order_details as $order_detail)
+								<img src="{{ asset('img/product') }}/{{ $order_detail->product->image }}" class="img-fluid" width="50">
+								{{ $order_detail->product->name }}
+								<br>
+								@endforeach
+							</td>
+							<td>
+								@if ($order->status == 1)
+								Belum Lunas
+								@else
+								Lunas
+								@endif
+							</td>
+							<td class="font-weight-bold">Rp. {{ number_format($order->total_price + $order->unique_code) }}</td>
+						</tr>
+						@empty
+						<tr>
+							<td colspan="6">Data Kosong</td>
+						</tr>
+						@endforelse
+						@endif
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+
+	<div class="row mt-4">
+		<div class="col">
+			<div class="card shadow-sm">
+				<div class="card-body">
+					<p>Untuk pembayaran silahkan dapat transfer di rekening dibawah ini : </p>
+					<div class="media">
+						<img class="mr-3" src="{{ asset('img/payment/bank_bca.png') }}" alt="Bank BCA" width="70">
+						<div class="media-body">
+							<h5 class="mt-0">BANK BCA</h5>
+							No. Rekening 28507823xx atas nama <span class="font-weight-bold">MUJAH ALBADRI</span>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
